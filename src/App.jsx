87ae7e9cd1b82f2f0ea8289200601cec7c21baf5
tammykhan <tammykhan543@gmail.com>
2025-10-23 11:31:22 +0600

    Added smooth dark mode toggle in Header component

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './components/Header';
+
+const App = () => {
+
+
+  return (
+    <div className='py-3 w-11/12 mx-auto  relative '>
+       <Header  />
+       <div className='border border-[#839FEE] my-5'></div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from 'react';
+import logo from "../assets/todo-logo.svg"
+import { IoMdMoon } from 'react-icons/io';
+import { IoSunny } from 'react-icons/io5';
+
+
+const Header = () => {
+
+   const [darkMode, setDarkMode] = useState(() => {
+    const savedTheme = localStorage.getItem("theme");
+    return savedTheme === "dark";
+  });
+
+  useEffect(() => {
+    const root = window.document.documentElement;
+    console.log("Dark mode:", darkMode);
+     if(darkMode){
+      root.classList.add("dark");
+      localStorage.setItem("theme", "dark")
+     } else {
+      root.classList.remove("dark");
+      localStorage.setItem("theme", "light")
+     }
+  }, [darkMode])
+
+
+  return (
+    <nav class="flex justify-between items-center">
+        <img className='w-36 md:w-40 ' src={logo} alt="BriskTodo" />
+
+         {/* Theme Toggle Button */}
+     <div onClick={() => setDarkMode(!darkMode) }
+       className='w-16 h-8 rounded-full bg-blue-400  flex justify-center items-center  cursor-pointer  '>
+        {/* sun icon */}
+      <div className={`text-white  w-8 h-8 flex justify-center items-center  rounded-full transition-all duration-150 ${!darkMode ? 'bg-blue-800 border-2 border-white' : 'bg-transparent' }`}>
+         <IoSunny className='w-4 h-5'/>
+        </div>
+        
+        {/* moon icon */}
+        <div className={`text-white w-8 h-8 flex justify-center items-center  rounded-full transition-all duration-150 ${darkMode ? 'bg-blue-800 border-2 border-white' : 'bg-transparent' } `}>
+         <IoMdMoon className='w-4 h-5'/>
+        </div>
+      </div>
+    
+      </nav>
+  );
+};
+
+export default Header;
